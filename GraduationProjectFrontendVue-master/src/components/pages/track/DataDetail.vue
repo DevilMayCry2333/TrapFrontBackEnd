7d6b59b5e40f871d324b2ef947fc94c4ed303750
DevilMayCry2333 <1712363499@qq.com>
2019-09-09 14:20:01 +0800

    9.9提交一发

--- GraduationProjectFrontendVue-master/src/components/pages/track/DataDetail.vue
+++ GraduationProjectFrontendVue-master/src/components/pages/track/DataDetail.vue
@@ -1,41 +1,40 @@
 <template>
 <div>
-          <el-table border :data="QRData.list" style="width: 100%" height="600">
-        <el-table-column prop="linename" label="线路名称" align="center"></el-table-column>
-        <el-table-column prop="timeconsume" label="耗时" align="center"></el-table-column>
-        <el-table-column prop="starttime" label="开始时间" align="center"></el-table-column>
-        <el-table-column prop="endtime" label="结束时间" align="center"></el-table-column>
-        <el-table-column prop="startpoint" label="起点(经纬度)" align="center"></el-table-column>
-        <el-table-column prop="endpoint" label="终点(经纬度)" align="center"></el-table-column>
-        <el-table-column prop="pic1" label="照片1" align="center"></el-table-column>
-        <el-table-column prop="pic2" label="照片2" align="center"></el-table-column>
-        <el-table-column prop="pic3" label="照片3" align="center"></el-table-column>
-        <el-table-column prop="pic4" label="照片4" align="center"></el-table-column>
-        <el-table-column prop="pic5" label="照片5" align="center"></el-table-column>
-        <el-table-column prop="worker" label="施工人员" align="center"></el-table-column>
-        <el-table-column prop="workingContent" label="工作内容" align="center"></el-table-column>
-        <el-table-column prop="remarks" label="备注" align="center"></el-table-column>
-        <el-table-column
-          prop="manager"
-          label="管理员"
-          align="center"
-          v-if="this.$store.state.user.role == 3"
-        ></el-table-column>
+            <el-table border :data="QRData.list" style="width: 100%" height="600">
+            <el-table-column prop="linename" label="线路名称" align="center"></el-table-column>
+            <el-table-column prop="timeconsume" label="耗时" align="center"></el-table-column>
+            <el-table-column prop="starttime" label="开始时间" align="center"></el-table-column>
+            <el-table-column prop="endtime" label="结束时间" align="center"></el-table-column>
+            <el-table-column prop="startpoint" label="起点(经纬度)" align="center"></el-table-column>
+            <el-table-column prop="endpoint" label="终点(经纬度)" align="center"></el-table-column>
+            <el-table-column prop="pic1" label="照片1" align="center"></el-table-column>
+            <el-table-column prop="pic2" label="照片2" align="center"></el-table-column>
+            <el-table-column prop="pic3" label="照片3" align="center"></el-table-column>
+            <el-table-column prop="pic4" label="照片4" align="center"></el-table-column>
+            <el-table-column prop="pic5" label="照片5" align="center"></el-table-column>
+            <el-table-column prop="worker" label="施工人员" align="center"></el-table-column>
+            <el-table-column prop="workingContent" label="工作内容" align="center"></el-table-column>
+            <el-table-column prop="remarks" label="备注" align="center"></el-table-column>
+            <el-table-column
+            prop="manager"
+            label="管理员"
+            align="center"
+            v-if="this.$store.state.user.role == 3"
+            ></el-table-column>
 
 
-      </el-table>
-        <div class="block">
-         <el-pagination
-            background
-            @current-change="handleQRDataCurrentPageChanged"
-            :current-page.sync="QRData.page"
-            :page-size="10"
-            layout="total, prev, pager, next"
-            :total="QRData.total"
-        ></el-pagination>
-        </div>
+        </el-table>
+            <div class="block">
+            <el-pagination
+                background
+                @current-change="handleQRDataCurrentPageChanged"
+                :current-page.sync="QRData.page"
+                :page-size="10"
+                layout="total, prev, pager, next"
+                :total="QRData.total"
+            ></el-pagination>
+            </div>
 </div>
-
 
 </template>
 
@@ -43,6 +42,8 @@
 import http from "../../../utils/http";
 export default {
     mounted(){
+        console.log(this.$store.state.user.role);
+        this.role = this.$store.state.user.role;
         this.loadDevice();
     },
     methods:{
@@ -57,9 +58,10 @@ export default {
       },
 
         loadDevice() {
+          console.log(this.role);
           console.log(sessionStorage['username']);
         http.requestWithToken(
-            "/track/detail",
+            "/track/areaDetail",
             "post",
             { page: this.QRData.page, limit: 10, username: sessionStorage['username']},
             res => {
@@ -73,13 +75,15 @@ export default {
     },
     data(){
         return{
+        role:'',
         QRData: {
             selectedIndex: -1,
             list: [],
             page: 1,
             limit: 10,
             total: 0
-        }
+        },
+
 
         }
     },
@@ -88,5 +92,26 @@ export default {
 </script>
 
 <style>
-
+#statistics-tabs .el-table thead {
+  color: black;
+  font-weight: 500;
+}
+#statistics-tabs .el-table__header-wrapper table {
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+}
+#statistics-tabs .el-table__body-wrapper {
+  padding-bottom: 5px;
+}
+#statistics-tabs .el-table__body-wrapper table {
+  border-bottom: 1px solid #000;
+}
+#statistics-tabs .el-table__body-wrapper tbody {
+  border-top-color: white;
+  border-bottom-color: white;
+}
+#statistics-tabs .el-table__body-wrapper tr {
+  border-top-color: white;
+  border-bottom-color: white;
+}
 </style>
