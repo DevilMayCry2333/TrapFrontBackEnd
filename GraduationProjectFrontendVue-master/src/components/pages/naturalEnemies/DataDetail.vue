1c33d93a432f5447cff99de6c27d39054ce8144d
DevilMayCry2333 <1712363499@qq.com>
2019-09-21 19:49:03 +0800

    天敌防治->数据明细->搜索

--- GraduationProjectFrontendVue-master/src/components/pages/naturalEnemies/DataDetail.vue
+++ GraduationProjectFrontendVue-master/src/components/pages/naturalEnemies/DataDetail.vue
@@ -1,5 +1,19 @@
 <template>
 <div>
+        <el-date-picker v-model="startDate" type="date" value-format="yyyy-MM-dd" placeholder="起始日期"></el-date-picker>
+      <el-date-picker v-model="endDate" type="date" value-format="yyyy-MM-dd" placeholder="终止日期"></el-date-picker>
+      <el-select v-model="value" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
+
+      <el-button type="primary" @click="query()">查询</el-button>
+
           <el-table border :data="QRData.list" style="width: 100%" height="600">
         <el-table-column prop="deviceId" label="设备ID" align="center"></el-table-column>
         <el-table-column prop="serial" label="编号" align="center"></el-table-column>
@@ -41,14 +55,74 @@
 import http from "../../../utils/http";
 export default {
     mounted(){
-        this.role = this.$store.state.user.role;
-        this.loadDevice();
+     console.log("init");
+      let role = this.$store.state.user.role;
+              this.loadDevice();
+      console.log(role);
+      if (role == 1) {
+        this.province = this.$store.state.user.adcode.substr(0, 2);
+      } else if (role == 2) {
+        this.province = this.$store.state.user.adcode.substr(0, 2);
+        this.city = this.$store.state.user.adcode.substr(0, 4);
+      } else if (role == 3) {
+        this.province = this.$store.state.user.adcode.substr(0, 2);
+        this.city = this.$store.state.user.adcode.substr(0, 4);
+        this.area = this.$store.state.user.adcode;
+            }
+            else if (role == 4) {
+                          this.province = this.$store.state.user.adcode.substr(0, 2);
+                          this.city = this.$store.state.user.adcode.substr(0, 4);
+                          this.area = this.$store.state.user.adcode;
+                          this.manager=this.$store.state.user.username;
+                        }
+                           console.log(this.city);
+                            console.log(this.province);
+                             console.log(this.area);
+
     },
     methods:{
-            handleQRDataCurrentPageChanged(val) {
+      handleQRDataCurrentPageChanged(val) {
         this.QRData.page = val;
         console.log("valChange" + val);
         this.loadDevice();
+      },
+      query(){
+        console.log("query");
+        console.log(this.startDate);
+        console.log(this.endDate);
+        console.log(this.input);
+        console.log(this.value);
+        let role = this.$store.state.user.role;
+          console.log(role);
+            console.log(this.area);
+            console.log(this.city);
+              console.log(this.province);
+          if(role==3){
+                        http.requestWithToken(
+                "/natural/searchDetail",
+                "post",
+                { 
+                  page: this.QRData.page, 
+                  limit: 10, 
+                  username: sessionStorage['username'],
+                  startDate: this.startDate,
+                  endDate: this.endDate,
+                  colName: this.value,
+                  searchText: this.input,
+                  adcode: this.area
+                },
+                res => {
+                  console.log(res);
+                this.QRData.list = res.data.Data;
+                this.QRData.total = res.data.total;
+
+                },
+                () => {}
+            );
+
+          }
+
+
       },
       handleQRDataCurrentPageChanged() {
         console.log("hello");
@@ -56,6 +130,10 @@ export default {
       },
 
         loadDevice() {
+          console.log("load");
+                let role = this.$store.state.user.role;
+                this.role = role;
+          console.log(this.role);
           if(this.role == 3){
               console.log(sessionStorage['username']);
             http.requestWithToken(
@@ -93,7 +171,28 @@ export default {
             page: 1,
             limit: 10,
             total: 0
-        }
+        },
+        startDate:'',
+        endDate: '',
+        options: [{
+          value: 'device_Id',
+          label: '编号'
+        }, {
+          value: 'region',
+          label: '区域'
+        }, {
+          value: 'batch',
+          label: '批次'
+        }, {
+          value: 'Worker',
+          label: '施工人员'
+        }],
+        value: '',
+        input:'',
+        province:'',
+        city:'',
+        area:'',
+        manager:''
 
         }
     },
