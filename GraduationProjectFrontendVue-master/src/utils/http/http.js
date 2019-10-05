3347ac3f45b51ec6423abd7524b6dba92052d374
DevilMayCry2333 <1712363499@qq.com>
2019-10-05 19:56:54 +0800

    最新

--- GraduationProjectFrontendVue-master/src/utils/http/http.js
+++ GraduationProjectFrontendVue-master/src/utils/http/http.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 
 //let BASE_URL = "https://39.108.184.47:8081";
 
-let BASE_URL = "http://localhost:8081";
+let BASE_URL = "http://106.15.90.78:8081";
 
 if (window.location.toString().indexOf('47.103.66.70') > -1) {
     //BASE_URL = "http://47.103.66.70:8081";
-   BASE_URL = "http://localhost:8081";
+   BASE_URL = "http://106.15.90.78:8081";
 } else {
-    // BASE_URL = "http://localhost:8081";
-    BASE_URL = "http://localhost:8081";
+    // BASE_URL = "http://106.15.90.78:8081";
+    BASE_URL = "http://106.15.90.78:8081";
 }
 
 
@@ -100,9 +100,9 @@ export const getBaseUrl = () => {
     let BASE_URL = "";
     if (window.location.toString().indexOf("47.103.66.70") > -1) {
        // BASE_URL = "http://47.103.66.70:8081";
-      BASE_URL = "http://localhost:8081";
+      BASE_URL = "http://106.15.90.78:8081";
     } else {
-        BASE_URL = "http://localhost:8081";
+        BASE_URL = "http://106.15.90.78:8081";
     }
     return BASE_URL;
 }
