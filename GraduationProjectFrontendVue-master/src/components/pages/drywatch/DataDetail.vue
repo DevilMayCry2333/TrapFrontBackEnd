7f5c2103dca87f376ae4a0af803aff216bf61359
ykaiyu <1712363499@qq.com>
2019-11-15 10:44:49 +0800

    注干剂图像下载

--- GraduationProjectFrontendVue-master/src/components/pages/drywatch/DataDetail.vue
+++ GraduationProjectFrontendVue-master/src/components/pages/drywatch/DataDetail.vue
@@ -17,6 +17,7 @@
         </div>
         <div style="display: flex;" class="divcss5-right">
           <el-button  type="primary" @click="exportExcel" style="height: fit-content;">导出</el-button>
+          <el-button  type="primary" @click="exportImage" style="height: fit-content;">导出图像</el-button>
             <!--<el-button type="primary" @click="someExportExcel">批量导出</el-button>-->
 
             <!--<el-button type="primary" @click="importExcel(scope.$index)">导入</el-button>-->
@@ -177,6 +178,66 @@
       this.loadDevice();
     },
     methods: {
+      exportImage(){
+                console.log("导出");
+      let role2 = this.$store.state.user.role;
+      this.role2 = role2;
+              // this.loadDevice();
+      console.log(role2);
+      if (role2 == 1) {
+        this.province = this.$store.state.user.adcode.substr(0, 2);
+      } else if (role2 == 2) {
+        this.province = this.$store.state.user.adcode.substr(0, 2);
+        this.city = this.$store.state.user.adcode.substr(0, 4);
+      } else if (role2 == 3) {
+        this.province = this.$store.state.user.adcode.substr(0, 2);
+        this.city = this.$store.state.user.adcode.substr(0, 4);
+        this.area = this.$store.state.user.adcode;
+            }
+            else if (role2 == 4) {
+                          this.province = this.$store.state.user.adcode.substr(0, 2);
+                          this.city = this.$store.state.user.adcode.substr(0, 4);
+                          this.area = this.$store.state.user.adcode;
+                          this.manager=this.$store.state.user.username;
+                        }
+
+        if(!this.value){
+          this.value = "";
+        }
+        if(!this.input){
+          this.input = "";
+        }
+        if(!this.startDate){
+          this.startDate = "";
+        }
+        if(!this.endDate){
+          this.endDate = "";
+        }
+        if(!this.area){
+          this.area = "";
+        }
+
+
+        setTimeout(()=>{
+                  window.location =
+        http.getBaseUrl() +
+        "/dryWatch/exportImage?startDate=" +
+        this.startDate +
+        "&endDate=" +
+        this.endDate +
+        "&searchText=" +
+        this.searchText +
+        "&colName=" +
+        this.selected +
+        "&adcode=" +
+        this.area +
+        "&username="+
+        sessionStorage['username'] +
+        "&token=" +
+        sessionStorage["token"];
+        },1000)
+
+      },
       handleEditMaintenanceDataSubmit(){
         console.log(this.EditMaintenanceDialog.form);
               http.requestWithTokenJson(
