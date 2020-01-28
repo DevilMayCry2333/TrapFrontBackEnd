3b03c361171c345c54425ae240ca608b8f0babe2
ykaiyu <1712363499@qq.com>
2020-01-28 20:36:35 +0800

    openlayers提交

--- GraduationProjectFrontendVue-master/src/components/pages/MyOpen/MyOpenLayers.vue
+++ GraduationProjectFrontendVue-master/src/components/pages/MyOpen/MyOpenLayers.vue
@@ -2,34 +2,89 @@
   <iframe ref="iframe" src="http://localhost:8080/customMap" style="width:100%;height:80%;" @load="test"></iframe>
 </template>
 <script>
+import http from "../../../utils/http";
+
 export default {
   mounted(){
 
   },
   methods:{
     test(){
+      this.$cookies.remove('province');
+      this.$cookies.remove('city');
+      this.$cookies.remove('area');
+      this.$cookies.remove('manager');
+      this.$cookies.remove('token');
+
           let role = this.$store.state.user.role;
       if (role == 1) {
         this.province = this.$store.state.user.adcode.substr(0, 2);
+         this.$cookies.set('province',this.province);
       } else if (role == 2) {
         this.province = this.$store.state.user.adcode.substr(0, 2);
         this.city = this.$store.state.user.adcode.substr(0, 4);
+         this.$cookies.set('province',this.province);
+        this.$cookies.set('city',this.city);
       } else if (role == 3) {
         this.province = this.$store.state.user.adcode.substr(0, 2);
         this.city = this.$store.state.user.adcode.substr(0, 4);
         this.area = this.$store.state.user.adcode;
+                 this.$cookies.set('province',this.province);
+        this.$cookies.set('city',this.city);
+              this.$cookies.set('area',this.area);
             }
             else if (role == 4) {
                           this.province = this.$store.state.user.adcode.substr(0, 2);
                           this.city = this.$store.state.user.adcode.substr(0, 4);
                           this.area = this.$store.state.user.adcode;
                           this.manager=this.$store.state.user.username;
+                                           this.$cookies.set('province',this.province);
+                              this.$cookies.set('city',this.city);
+                                    this.$cookies.set('area',this.area);
+                            this.$cookies.set('manager',this.manager);
                         }
-                               
-                               
-                               
-                               
-      this.$cookies.set('area',this.area);
+
+                        this.$cookies.set('token',sessionStorage['token']);
+                                http.requestWithToken(
+                                  "/auth_api/device_list",
+                                      "get",
+                                      { 
+                                        page: 1, limit: 1000,
+                                        isMap:true
+                                      },
+                                      res => {
+                                        var count = 0;             
+                                        // console.log(res.data);
+                                        for(var i = 0 ; i < res.data.data.length; i++){
+                                          // console.log(res.data.data[i]);
+                                          
+                                          if(res.data.data[i].longitude!=null){
+                                            count++;
+                                          }
+                                        }
+                                        console.log("cnt" + count);
+                                        var tmp = [];
+                                        console.log("tmp" + tmp);
+
+                                        for(var i = 0 ; i < res.data.data.length; i++){
+                                          if(res.data.data[i].longitude!=null){
+                                            var json = {
+                                              "longitude":res.data.data[i].longitude,
+                                              "latitude":res.data.data[i].latitude
+                                            }
+                                            tmp.push(json);
+                                          }
+                                        }
+                                        console.log("tmp");
+                                        console.log(tmp);
+                                        var objString = JSON.stringify(tmp);
+                                        // this.$cookies.set('device',objString);
+                                        localStorage.setItem('device',objString);
+
+                                      },
+                                      () => {}
+                                );
+
     }
   },
   data(){
