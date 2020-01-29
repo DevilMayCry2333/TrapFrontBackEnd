38361dcbda162a2cc118bb5bd74158fc2752c8e4
ykaiyu <1712363499@qq.com>
2020-01-29 12:34:58 +0800

    最新

--- GraduationProjectFrontendVue-master/src/components/pages/MyOpen/MyOpenLayers.vue
+++ GraduationProjectFrontendVue-master/src/components/pages/MyOpen/MyOpenLayers.vue
@@ -1,12 +1,13 @@
 <template>
-  <iframe ref="iframe" src="http://106.15.200.245/test2.html" style="width:100%;height:100%;" @load="test"></iframe>
+  <iframe ref="iframe" src="http://localhost:8080/customMap" style="width:100%;height:100%;"></iframe>
+  <!-- <iframe ref="iframe" src="http://106.15.200.245/test2.html" style="width:100%;height:100%;"></iframe> -->
 </template>
 <script>
 import http from "../../../utils/http";
 
 export default {
-  mounted(){
-
+  created(){
+    this.test();
   },
   methods:{
     test(){
